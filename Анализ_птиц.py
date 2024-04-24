8f5532ea1a75bc9002eaea4afef777a4c5dced3e
sanumxxx <ytsansyt@gmail.com>
2024-04-24 23:48:37 +0300

    v1.5 Удалены обновления приложение, для следующих релизов

--- Анализ_птиц.py
+++ Анализ_птиц.py
@@ -2,64 +2,20 @@ import tkinter as tk
 from tkinter import ttk
 import pandas as pd
 import matplotlib.pyplot as plt
-import requests
-import subprocess
-import git
+from tkinter import messagebox
 import os
-import zipfile
 
+current_version = 'v1.5'
 
-current_version = 'v1.2'
-github_repo = 'https://api.github.com/repos/sanumxxx/BirdsApp/releases/latest'
-
-
-# Функция для проверки обновлений на GitHub
-def check_for_updates(current_version):
-    try:
-        response = requests.get(github_repo)
-        data = response.json()
-        latest_version = data['tag_name']
-        assets = data.get('assets', [])
-        if latest_version != current_version and assets:
-            print("Доступно обновление.")
-            return True, assets[0]['browser_download_url']
-        else:
-            print("У вас установлена последняя версия.")
-            return False, None
-    except Exception as e:
-        print("Ошибка при проверке обновлений:", str(e))
-        return False, None
-
-
-# Функция для загрузки обновлений с GitHub
-def update_from_github(download_url):
-    try:
-        local_filename = download_url.split('/')[-1]
-        # Скачиваем файл
-        with requests.get(download_url, stream=True) as r:
-            r.raise_for_status()
-            with open(local_filename, 'wb') as f:
-                for chunk in r.iter_content(chunk_size=8192):
-                    f.write(chunk)
-        # Распаковываем архив
-        with zipfile.ZipFile(local_filename, 'r') as zip_ref:
-            zip_ref.extractall(os.getcwd())
-        os.remove(local_filename)
-        print("Программа успешно обновлена. Пожалуйста, перезапустите ее.")
-    except Exception as e:
-        print("Ошибка при обновлении программы:", str(e))
-
-
+root = tk.Tk()
+root.title("Анализ птиц")
 
 # Загрузка данных
 file_path = 'botiev2017.xlsx'
 data = pd.read_excel(file_path)
 
-
-
 # Преобразование дат
 data['Дата-время наблюдения'] = pd.to_datetime(data['Дата-время наблюдения'], errors='coerce')
-
 
 # Определение сезона по месяцу
 def get_season(date):
@@ -73,9 +29,7 @@ def get_season(date):
     else:
         return 'Зима'
 
-
 data['Сезон'] = data['Дата-время наблюдения'].apply(get_season)
-
 
 # Функция для обновления таблицы
 def update_table(filtered_data):
@@ -83,7 +37,6 @@ def update_table(filtered_data):
         tree.delete(row)
     for _, row in filtered_data.iterrows():
         tree.insert("", "end", values=list(row))
-
 
 # Функция фильтрации данных
 def filter_data():
@@ -95,12 +48,8 @@ def filter_data():
     if season != "Весь период":
         filtered_data = filtered_data[filtered_data['Сезон'] == season]
     update_table(filtered_data)
-
     update_status(filtered_data)
 
-
-# Функция для отображения гистограммы
-# Функция для отображения гистограммы
 # Функция для отображения гистограммы
 def show_histogram():
     bird_type = bird_var.get()
@@ -111,7 +60,7 @@ def show_histogram():
     if season != "Весь период":
         filtered_data = filtered_data[filtered_data['Сезон'] == season]
 
-    # Создаем гистограмму, где вес каждого наблюдения равен количеству птиц в миграции
+    # Создаем гистограмму
     counts, bins, patches = plt.hist(filtered_data['Высота миграции, метров'],
                                      weights=filtered_data['Кол-во птиц в миграции'],
                                      bins=20, color='blue', alpha=0.7)
@@ -119,45 +68,27 @@ def show_histogram():
     plt.title(f'Распределение высоты полета для {bird_type}, {season}')
     plt.xlabel('Высота миграции, метров')
     plt.ylabel('Количество птиц')
-
-    # Добавляем текст над столбцами
-    for count, bin_edge in zip(counts, bins):
-        if count > 0:
-            plt.text(bin_edge, count, str(int(count)), ha='center', va='bottom')
-
     plt.show()
 
-# Остальная часть вашего кода...
-
+# Функция для обновления статуса
 def update_status(filtered_data):
     total_birds = filtered_data['Кол-во птиц в миграции'].sum()
     total_species = filtered_data['Вид птицы'].nunique()
     status_label.config(text=f'Всего птиц: {total_birds}, Всего видов: {total_species}')
 
-root = tk.Tk()
-root.title("Анализ птиц")
-
+# Интерфейс пользователя
 status_frame = ttk.Frame(root)
 status_frame.pack(side=tk.BOTTOM, fill=tk.X, padx=10, pady=5)
 
 status_label = ttk.Label(status_frame, text='Всего птиц: 0, Всего видов: 0')
 status_label.pack(side=tk.LEFT)
 
-# Панель версии для отображения версии приложения
 version_label = ttk.Label(status_frame, text=current_version)
 version_label.pack(side=tk.RIGHT)
 
-
-
-
-# Создание основного окна
-
-
-# Панель инструментов для выпадающих меню и кнопок
 toolbar = ttk.Frame(root)
 toolbar.pack(side=tk.TOP, fill=tk.X, padx=10, pady=5)
 
-# Выпадающие меню для выбора видов птиц и сезонов
 bird_var = tk.StringVar(root)
 bird_options = ["Все виды"] + sorted(data['Вид птицы'].unique().tolist())
 bird_menu = ttk.OptionMenu(toolbar, bird_var, bird_options[0], *bird_options)
@@ -168,15 +99,12 @@ season_options = ["Весь период"] + sorted(data['Сезон'].unique().
 season_menu = ttk.OptionMenu(toolbar, season_var, season_options[0], *season_options)
 season_menu.pack(side=tk.LEFT)
 
-# Кнопка загрузить
 load_button = ttk.Button(toolbar, text="Загрузить", command=filter_data)
 load_button.pack(side=tk.LEFT)
 
-# Таблица для отображения данных
 table_frame = ttk.Frame(root, relief=tk.RAISED, borderwidth=1)
 table_frame.pack(fill=tk.BOTH, expand=True, padx=10, pady=10)
 
-# Создаем вертикальный и горизонтальный скроллбары
 vertical_scrollbar = ttk.Scrollbar(table_frame, orient="vertical")
 horizontal_scrollbar = ttk.Scrollbar(table_frame, orient="horizontal")
 
@@ -188,7 +116,6 @@ for col in data.columns:
     tree.heading(col, text=col)
     tree.column(col, anchor='center')
 
-# Устанавливаем скроллбары
 vertical_scrollbar.config(command=tree.yview)
 horizontal_scrollbar.config(command=tree.xview)
 
@@ -197,19 +124,11 @@ horizontal_scrollbar.pack(side="bottom", fill="x")
 
 tree.pack(fill=tk.BOTH, expand=True, padx=10, pady=10)
 
-# Кнопка для отображения гистограммы
-
 graph_button_frame = ttk.Frame(root)
 graph_button_frame.pack(side=tk.LEFT, fill=tk.X, padx=10, pady=5)
 
-
 hist_button = ttk.Button(graph_button_frame, text="График", command=show_histogram)
 hist_button.pack(side=tk.LEFT, padx=10, pady=10)
-
-
-update_available, download_url = check_for_updates(current_version)
-if update_available:
-    update_from_github(download_url)
 
 # Запуск основного цикла обработки событий
 root.mainloop()
